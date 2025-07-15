10f8fffa0c88bc8de704ec9ff20e3d578d4137b3
Kyohei-Omukai <kyohai.omukai@reactive-design.com>
2025-07-16 01:43:16 +0900

    縦型に変更

--- components/Info.js
+++ components/Info.js
@@ -3,22 +3,29 @@ import Image from 'next/image'
 import styles from 'styles/Info.module.scss'
 
 export default function Info({ detail }) {
+    const formattedHoliday = '定休日：' + detail.holiday.replace(/\n/g, '<br>')
+    const formattedHour = '営業時間：' + detail.hours.replace(/\n/g, '<br>')
+
     return (
-        <div className={`${styles.basicInfo} stack`}>
-            <h2 className={styles.title}>{detail.galleryName}</h2>
-            <ul className="stack-min">
-                <li className="note">定休日：{detail.holiday}</li>
-                <li className="note">営業時間：{detail.hours}</li>
-                <li className={styles.address}>
+        <div className={styles.basicInfo}>
+            <div className="stack">
+                <h2 className={styles.title}>{detail.galleryName}</h2>
+                <p className={styles.address}>
                     〒{detail.zip}
                     <br />
                     {detail.address}
-                </li>
-            </ul>
-            <p className={styles.tel}>
-                <Image src="/tel-img.svg" alt="" width="41" height="33" />
-                {detail.tel}
-            </p>
+                </p>
+            </div>
+            <div className="stack">
+                <p className={styles.tel}>
+                    <Image src="/tel-img.svg" alt="" width="41" height="33" />
+                    {detail.tel}
+                </p>
+                <ul className="stack-min">
+                    <li className="note" dangerouslySetInnerHTML={{ __html: formattedHoliday }}></li>
+                    <li className="note" dangerouslySetInnerHTML={{ __html: formattedHour }}></li>
+                </ul>
+            </div>
         </div>
     )
 }
